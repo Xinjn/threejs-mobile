2eae0d72451c12865513f8d64fe4111b87e6070a
xinjn <xinjn@ifeng.com>
2022-07-20 19:28:17 +0800

    feat: init

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // 关闭严格模式
+  // 部署路径
+  publicPath: process.env.NODE_ENV === "production" ? "/threejs-mobile/" : "/",
 });
